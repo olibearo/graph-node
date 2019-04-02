da0dcaa0503e8fbeed51de42f83d51caba98a504
Jorge Olivero <jorgeaolivero@gmail.com>
2019-04-02 14:31:50 -0500

    datasource/ethereum, graph, runtime/wasm: fixing block_handlers being optional

--- graph/src/data/subgraph/schema.rs
+++ graph/src/data/subgraph/schema.rs
@@ -627,7 +627,7 @@ struct EthereumContractMappingEntity {
     pub file: String,
     pub entities: Vec<String>,
     pub abis: Vec<EthereumContractAbiEntity>,
-    pub block_handlers: EthereumBlockHandlerEntity,
+    pub block_handlers: Option<EthereumBlockHandlerEntity>,
     pub transaction_handlers: Vec<EthereumTransactionHandlerEntity>,
     pub event_handlers: Vec<EthereumContractEventHandlerEntity>,
 }
@@ -685,7 +685,9 @@ impl<'a> From<&'a super::Mapping> for EthereumContractMappingEntity {
             file: mapping.link.link.clone(),
             entities: mapping.entities.clone(),
             abis: mapping.abis.iter().map(Into::into).collect(),
-            block_handlers: mapping.block_handlers.clone().into(),
+            block_handlers: mapping.block_handlers
+                .clone()
+                .map(|block_handler| block_handler.into()),
             transaction_handlers: mapping
                 .transaction_handlers
                 .clone()
