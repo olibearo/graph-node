95ea07254ca268589608c0965f7535ded1b4c79e
Jorge Olivero <jorgeaolivero@gmail.com>
2019-04-02 21:42:12 -0500

    core, datasource/ethereum, graph, runtime/wasm: finish process_block

--- datasource/ethereum/src/block_stream.rs
+++ datasource/ethereum/src/block_stream.rs
@@ -1323,7 +1323,7 @@ fn parse_triggers(
                     return Err(format_err!("Unable to determine transaction index for Ethereum call."))
                 }
             }
-            EthereumTrigger::Block => continue,
+            EthereumTrigger::Block(_) => continue,
         }
     }
 
@@ -1408,7 +1408,7 @@ fn parse_block_triggers(
             call_filter.matches(call)
         })
         .map(move |call| {
-            EthereumTrigger::Block
+            EthereumTrigger::Block(call.clone())
         })
         .collect()
 }
