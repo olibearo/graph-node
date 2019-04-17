00f7549fd19805fb2c3109569223db1c7c1ecc6c
Jorge Olivero <jorgeaolivero@gmail.com>
2019-04-17 12:17:39 -0500

    core: Update error to use `event` instead of `log`

--- core/src/subgraph/instance.rs
+++ core/src/subgraph/instance.rs
@@ -97,7 +97,7 @@ where
                 let transaction = block
                     .transaction_for_log(&log)
                     .map(Arc::new)
-                    .ok_or_else(|| format_err!("Found no transaction for log"));
+                    .ok_or_else(|| format_err!("Found no transaction for event"));
                 let matching_hosts: Vec<_> = hosts
                     .iter()
                     .filter(|host| host.matches_log(&log))
